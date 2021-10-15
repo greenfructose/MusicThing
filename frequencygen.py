136c61059596641dbe04cde557dcff40a49ced21
greenfructose <justinclarkturney@gmail.com>
2021-10-15 15:38:47 -0500

    Removed old print statements

--- frequencygen.py
+++ frequencygen.py
@@ -7,14 +7,11 @@ def get_frequencies(start, end):
     octave_count = 0
     frequency_dict = {}
     while current_frequency < end:
-        # print(str(current_frequency) + f' {notes[0]}{octave_count}')
         frequency_dict[f'{notes[0]}{octave_count}'] = current_frequency
         for i in range(1, 12):
-            # print(str(current_frequency * 2 ** (i / 12.0)) + f' {notes[i]}{octave_count}')
             frequency_dict[f'{notes[i]}{octave_count}'] = current_frequency * 2 ** (i / 12.0)
         current_frequency *= 2
         octave_count += 1
-    # print(str(current_frequency) + f' {notes[0]}{octave_count}')
     frequency_dict[f'{notes[0]}{octave_count}'] = current_frequency
     return frequency_dict
 
