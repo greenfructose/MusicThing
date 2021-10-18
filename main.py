a8186571196e08f829ac31b56577b785dbdbc19a
greenfructose <justinclarkturney@gmail.com>
2021-10-18 16:58:16 -0500

    Testing chord sounds

--- main.py
+++ main.py
@@ -320,184 +320,193 @@ seven_sus_four = [P1, P4, P5, m7]
 add_nine = [P1, M3, P5, M9]
 add_eleven = [P1, M3, P5, P11]
 
-pprint.pprint(get_scale_frequencies(note_frequencies['c4'], 'dorian'))
 
-# class Oscillator(ABC):
-#     def __init__(self, freq=440.0, phase=0, amp=1.0, sample_rate=44_100, wave_range=(-1, 1)):
-#         self._freq = freq
-#         self._amp = amp
-#         self._phase = phase
-#         self._sample_rate = sample_rate
-#         self._wave_range = wave_range
-#
-#         # Properties that will be changed
-#         self._f = freq
-#         self._a = amp
-#         self._p = phase
-#
-#     @property
-#     def init_freq(self):
-#         return self._freq
-#
-#     @property
-#     def init_amp(self):
-#         return self._amp
-#
-#     @property
-#     def init_phase(self):
-#         return self._phase
-#
-#     @property
-#     def freq(self):
-#         return self._f
-#
-#     @freq.setter
-#     def freq(self, value):
-#         self._f = value
-#         self._post_freq_set()
-#
-#     @property
-#     def amp(self):
-#         return self._a
-#
-#     @amp.setter
-#     def amp(self, value):
-#         self._a = value
-#         self._post_amp_set()
-#
-#     @property
-#     def phase(self):
-#         return self._p
-#
-#     @phase.setter
-#     def phase(self, value):
-#         self._p = value
-#         self._post_phase_set()
-#
-#     def _post_freq_set(self):
-#         pass
-#
-#     def _post_amp_set(self):
-#         pass
-#
-#     def _post_phase_set(self):
-#         pass
-#
-#     @abstractmethod
-#     def _initialize_osc(self):
-#         pass
-#
-#     @staticmethod
-#     def squish_val(val, min_val=0, max_val=1):
-#         return (((val + 1) / 2) * (max_val - min_val)) + min_val
-#
-#     @abstractmethod
-#     def __next__(self):
-#         return None
-#
-#     def __iter__(self):
-#         self.freq = self._freq
-#         self.phase = self._phase
-#         self.amp = self._amp
-#         self._initialize_osc()
-#         return self
-#
-#
-# class SineOscillator(Oscillator):
-#     def _post_freq_set(self):
-#         self._step = (2 * math.pi * self._f) / self._sample_rate
-#
-#     def _post_phase_set(self):
-#         self._p = (self._p / 360) * 2 * math.pi
-#
-#     def _initialize_osc(self):
-#         self._i = 0
-#
-#     def __next__(self):
-#         val = math.sin(self._i + self._p)
-#         self._i = self._i + self._step
-#         if self._wave_range is not (-1, 1):
-#             val = self.squish_val(val, *self._wave_range)
-#         return val * self._a
-#
-#
-# class SquareOscillator(SineOscillator):
-#     def __init__(self, freq=440, phase=0, amp=1, sample_rate=44_100, wave_range=(-1, 1), threshold=0):
-#         super().__init__(freq, phase, amp, sample_rate, wave_range)
-#         self.threshold = threshold
-#
-#     def __next__(self):
-#         val = math.sin(self._i + self._p)
-#         self._i = self._i + self._step
-#         if val < self.threshold:
-#             val = self._wave_range[0]
-#         else:
-#             val = self._wave_range[1]
-#         return val * self._a
-#
-#
-# class SawtoothOscillator(Oscillator):
-#     def _post_freq_set(self):
-#         self._period = self._sample_rate / self._f
-#         self._post_phase_set
-#
-#     def _post_phase_set(self):
-#         self._p = ((self._p + 90) / 360) * self._period
-#
-#     def _initialize_osc(self):
-#         self._i = 0
-#
-#     def __next__(self):
-#         div = (self._i + self._p) / self._period
-#         val = 2 * (div - math.floor(0.5 + div))
-#         self._i = self._i + 1
-#         if self._wave_range is not (-1, 1):
-#             val = self.squish_val(val, *self._wave_range)
-#         return val * self._a
-#
-#
-# class TriangleOscillator(SawtoothOscillator):
-#     def __next__(self):
-#         div = (self._i + self._p) / self._period
-#         val = 2 * (div - math.floor(0.5 + div))
-#         val = (abs(val) - 0.5) * 2
-#         self._i = self._i + 1
-#         if self._wave_range is not (-1, 1):
-#             val = self.squish_val(val, *self._wave_range)
-#         return val * self._a
-#
-#
-# class WaveAdder:
-#     def __init__(self, *oscillators):
-#         self.oscillators = oscillators
-#         self.n = len(oscillators)
-#
-#     def __iter__(self):
-#         [iter(osc) for osc in self.oscillators]
-#         return self
-#
-#     def __next__(self):
-#         return sum(next(osc) for osc in self.oscillators) / self.n
-#
-#
-# def wave_to_file(wav, wav2=None, fname="temp.wav", amp=0.1, sample_rate=44100):
-#     wav = np.array(wav)
-#     wav = np.int16(wav * amp * (2 ** 15 - 1))
-#
-#     if wav2 is not None:
-#         wav2 = np.array(wav2)
-#         wav2 = np.int16(wav2 * amp * (2 ** 15 - 1))
-#         wav = np.stack([wav, wav2]).T
-#
-#     wavfile.write(fname, sample_rate, wav)
+def get_chord_frequencies(root, chord):
+    chord_frequencies = []
+    for note in chord:
+        chord_frequencies.append(get_interval_frequency(root, note))
+    return chord_frequencies
 
-#
-# gen = WaveAdder(
-#     SineOscillator(freq=note_frequencies['c3']),
-#     TriangleOscillator(freq=note_frequencies['e3'], amp=0.8),
-#     SawtoothOscillator(freq=note_frequencies['g3'], amp=0.6),
-#     SquareOscillator(freq=note_frequencies['c2'], amp=0.4),
-# )
-# iter(gen)
-# wav = [next(gen) for _ in range(44100 * 4)]  # 4 Seconds
-# wave_to_file(wav, fname="prelude_one.wav")
+
+pprint.pprint(get_chord_frequencies(note_frequencies['c4'], add_nine))
+
+class Oscillator(ABC):
+    def __init__(self, freq=440.0, phase=0, amp=1.0, sample_rate=44_100, wave_range=(-1, 1)):
+        self._freq = freq
+        self._amp = amp
+        self._phase = phase
+        self._sample_rate = sample_rate
+        self._wave_range = wave_range
+
+        # Properties that will be changed
+        self._f = freq
+        self._a = amp
+        self._p = phase
+
+    @property
+    def init_freq(self):
+        return self._freq
+
+    @property
+    def init_amp(self):
+        return self._amp
+
+    @property
+    def init_phase(self):
+        return self._phase
+
+    @property
+    def freq(self):
+        return self._f
+
+    @freq.setter
+    def freq(self, value):
+        self._f = value
+        self._post_freq_set()
+
+    @property
+    def amp(self):
+        return self._a
+
+    @amp.setter
+    def amp(self, value):
+        self._a = value
+        self._post_amp_set()
+
+    @property
+    def phase(self):
+        return self._p
+
+    @phase.setter
+    def phase(self, value):
+        self._p = value
+        self._post_phase_set()
+
+    def _post_freq_set(self):
+        pass
+
+    def _post_amp_set(self):
+        pass
+
+    def _post_phase_set(self):
+        pass
+
+    @abstractmethod
+    def _initialize_osc(self):
+        pass
+
+    @staticmethod
+    def squish_val(val, min_val=0, max_val=1):
+        return (((val + 1) / 2) * (max_val - min_val)) + min_val
+
+    @abstractmethod
+    def __next__(self):
+        return None
+
+    def __iter__(self):
+        self.freq = self._freq
+        self.phase = self._phase
+        self.amp = self._amp
+        self._initialize_osc()
+        return self
+
+
+class SineOscillator(Oscillator):
+    def _post_freq_set(self):
+        self._step = (2 * math.pi * self._f) / self._sample_rate
+
+    def _post_phase_set(self):
+        self._p = (self._p / 360) * 2 * math.pi
+
+    def _initialize_osc(self):
+        self._i = 0
+
+    def __next__(self):
+        val = math.sin(self._i + self._p)
+        self._i = self._i + self._step
+        if self._wave_range is not (-1, 1):
+            val = self.squish_val(val, *self._wave_range)
+        return val * self._a
+
+
+class SquareOscillator(SineOscillator):
+    def __init__(self, freq=440, phase=0, amp=1, sample_rate=44_100, wave_range=(-1, 1), threshold=0):
+        super().__init__(freq, phase, amp, sample_rate, wave_range)
+        self.threshold = threshold
+
+    def __next__(self):
+        val = math.sin(self._i + self._p)
+        self._i = self._i + self._step
+        if val < self.threshold:
+            val = self._wave_range[0]
+        else:
+            val = self._wave_range[1]
+        return val * self._a
+
+
+class SawtoothOscillator(Oscillator):
+    def _post_freq_set(self):
+        self._period = self._sample_rate / self._f
+        self._post_phase_set
+
+    def _post_phase_set(self):
+        self._p = ((self._p + 90) / 360) * self._period
+
+    def _initialize_osc(self):
+        self._i = 0
+
+    def __next__(self):
+        div = (self._i + self._p) / self._period
+        val = 2 * (div - math.floor(0.5 + div))
+        self._i = self._i + 1
+        if self._wave_range is not (-1, 1):
+            val = self.squish_val(val, *self._wave_range)
+        return val * self._a
+
+
+class TriangleOscillator(SawtoothOscillator):
+    def __next__(self):
+        div = (self._i + self._p) / self._period
+        val = 2 * (div - math.floor(0.5 + div))
+        val = (abs(val) - 0.5) * 2
+        self._i = self._i + 1
+        if self._wave_range is not (-1, 1):
+            val = self.squish_val(val, *self._wave_range)
+        return val * self._a
+
+
+class WaveAdder:
+    def __init__(self, *oscillators):
+        self.oscillators = oscillators
+        self.n = len(oscillators)
+
+    def __iter__(self):
+        [iter(osc) for osc in self.oscillators]
+        return self
+
+    def __next__(self):
+        return sum(next(osc) for osc in self.oscillators) / self.n
+
+
+def wave_to_file(wav, wav2=None, fname="temp.wav", amp=0.1, sample_rate=44100):
+    wav = np.array(wav)
+    wav = np.int16(wav * amp * (2 ** 15 - 1))
+
+    if wav2 is not None:
+        wav2 = np.array(wav2)
+        wav2 = np.int16(wav2 * amp * (2 ** 15 - 1))
+        wav = np.stack([wav, wav2]).T
+
+    wavfile.write(fname, sample_rate, wav)
+
+chord = get_chord_frequencies(note_frequencies['c5'], major_seventh)
+
+gen = WaveAdder(
+    SineOscillator(freq=chord[0]),
+    TriangleOscillator(freq=chord[1], amp=0.8),
+    SawtoothOscillator(freq=chord[2], amp=0.6),
+    SquareOscillator(freq=chord[3], amp=0.4),
+)
+iter(gen)
+wav = [next(gen) for _ in range(44100 * 4)]  # 4 Seconds
+wave_to_file(wav, fname="prelude_one.wav")
